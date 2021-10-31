0e90b2a10460c9d541fe16c885c39f743571c1c3
Theo Baker <tabsterbaker@gmail.com>
2021-10-31 00:14:32 -0400

    added CV

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3340d1df2fd2b732dc58"; }
+/******/ 		__webpack_require__.h = function() { return "eb34350bb9900b8e4e2e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Navbar.js
+++ components/Navbar.js
@@ -137,6 +137,32 @@ export default function Navbar() {
               )}
             </a>
           </Link>
+          <Link href="https://drive.google.com/file/d/1Cg-bs19BCxN5_bZeL_eLuSJhKaHp2iD8/view?usp=sharing">
+            <a
+              className={`text-base  ${
+                router.asPath === "/contact"
+                  ? "text-gray-800 font-bold dark:text-gray-400"
+                  : "text-gray-600 dark:text-gray-300 font-normal "
+              }`}
+            >
+              CV
+              {router.asPath === "/contact" && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-down inline-block h-3 w-3"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                  />
+                </svg>
+              )}
+            </a>
+          </Link>
         </div>
 
         <div className="space-x-4 flex flex-row items-center">
